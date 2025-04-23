b8e438c5668a39c7da8927e248cd8d897ced8d1d
Norbu Sherpa <sherpanorbu2000@gmail.com>
2025-04-23 16:33:26 -0400

    shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./routes/home/home";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation";
 import Authentication from "./routes/authentication/authentication";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Authentication />} />
+        <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
   );
