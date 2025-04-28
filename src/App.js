5dc33d605701834bf00aed0e076a1e360cc6ae55
Norbu Sherpa <sherpanorbu2000@gmail.com>
2025-04-28 16:50:07 -0400

    checkout page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation";
 import Authentication from "./routes/authentication/authentication";
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/auth" element={<Authentication />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
